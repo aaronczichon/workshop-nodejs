ae8d52a0e15847ae82354b2ec492870e7450ed0c
Aaron Czichon <mail@aaronczichon.de>
2023-10-03 16:56:31 +0200

    Task 5: Solution

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const filePath = path.join(__dirname, 'Task3_Bericht_Autohersteller.txt');
 
-fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
-  console.log(data);
-});
+const readReportFile = async (path) => {
+  return fs.readFile(path, 'utf8').catch(err => console.log(err));
+};
 
-const data = fs.readFileSync(filePath, {encoding: 'utf-8'});
-console.log(data);
-
-const text = 'Beispieldatei für den NodeJS Workshop';
-
-const newContent = `${text} \r\n${data}`;
-fs.writeFile(filePath, newContent, {
-  encoding: 'utf-8',
-  flag: 'w'
-}, (err) => {
-  console.log('Datei wurde geschrieben');
-});
+readReportFile(filePath).then(data => console.log(data));
