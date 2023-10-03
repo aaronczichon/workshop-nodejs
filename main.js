44c2b6c53eb5b57459b2d80fbb5c4a85a7285b86
Aaron Czichon <mail@aaronczichon.de>
2023-10-03 15:49:26 +0200

    Task 4: Solution

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, 'Task3_Bericht_Autohersteller.txt');
-const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
 
-readStream.on('data', (chunk) => {
-    console.log(chunk);
+fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
+  console.log(data);
 });
 
-readStream.on('error', (error) => {
-    console.log(error);
+const data = fs.readFileSync(filePath, {encoding: 'utf-8'});
+console.log(data);
+
+const text = 'Beispieldatei für den NodeJS Workshop';
+
+const newContent = `${text} \r\n${data}`;
+fs.writeFile(filePath, newContent, {
+  encoding: 'utf-8',
+  flag: 'w'
+}, (err) => {
+  console.log('Datei wurde geschrieben');
 });
-
-readStream.on('end', () => {
-    console.log('End of file');
-});
-
-setTimeout(() => readStream.pause(), 5);
-
-setTimeout(() => readStream.resume(), 2000);
